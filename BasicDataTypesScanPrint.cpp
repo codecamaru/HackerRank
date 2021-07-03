5c7d86535bb9fa03ecf54cf28f07aa82aa78ba0d
Carolina Marugan Rubio <54485798+codecamaru@users.noreply.github.com>
2021-07-03 17:07:47 +0200

    Update BasicDataTypesScanPrint.cpp

--- BasicDataTypesScanPrint.cpp
+++ BasicDataTypesScanPrint.cpp
@@ -8,6 +8,7 @@ int main() {
     char ch;
     float f;
     double d;
+    cout << "Introduce int, long, character, float and double separated by spaces" <<endl;
     scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
     //print float with only 3 decimals and double with 9
     printf("%d\n%ld\n%c\n%.3f\n%.9lf\n", i, l, ch, f, d);
